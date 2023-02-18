5e633f3b0d897c135ea5c4951fc7f4f1718d1618
bagse <102260190+Bagse@users.noreply.github.com>
2023-02-18 01:11:15 -0500

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { CharacterList } from "./components/CharacterList";
-import './App.css'
+import { Footer } from "./components/Footer";
+import './App.css';
+import './components/Footer.css';
 
 function App() {
   return <div className="bg-dark text-white">
     <h1 className="title text-info text-center display-1 py-4 ">Rick and Morty</h1>
     <CharacterList/>
+
+    <Footer />
   </div>;
 }
 
