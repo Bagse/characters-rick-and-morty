521726b8664dc11f62a3890d16db88082d16b2bf
bagse <102260190+Bagse@users.noreply.github.com>
2023-02-18 13:37:47 -0500

    logo linkedin fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
-
 export function Footer() {
-    return (
-      <footer className="footer mt-5 py-4 bg-dark ">
-        <div className="container flex-row  justify-content-between text-center">
-          <span className="p-3">Creado por Bagse &copy;</span>
-          <a href="https://www.linkedin.com/in/brian-ar%C3%B3n-g%C3%B3mez-sequeiros/">
-            <img className="img-footer" src="./src/assets/linkedin.png" alt="LinkedIn de Bagse" />
-          </a>
-        </div>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="footer mt-5 py-4 bg-dark ">
+      <div className="container flex-row  justify-content-between text-center">
+        <span className="p-3">Creado por Bagse &copy;</span>
+        <a href="https://www.linkedin.com/in/brian-ar%C3%B3n-g%C3%B3mez-sequeiros/">
+          <img
+            className="img-footer"
+            src="./img/linkedin.png"
+            alt="Linkedin de Bagse"
+          />
+        </a>
+      </div>
+    </footer>
+  );
+}
